c7cbf3e5e90cb639841da546a4b5114e97e892b5
Christian V <amnesiacsloth@gmail.com>
2024-03-17 16:26:05 -0700

    fixing test suite. IN progress

--- arrayz/asVectorTest.cpp
+++ arrayz/asVectorTest.cpp
@@ -9,18 +9,17 @@ protected:
     asVector slothVec;
     // SetUp() invoked for each test individually
     // Meant to initialize common resources
-    void SetUp() override {
-        slothVec = new asVector();
-    }
 
     // TearDown() invoked at end of each test or at point of failure(?)
     // Cleans up resources utilized
     // Teardown not needed since destructor does everything
-    TEST_F(asVectorTest, isEmptyInitially) {
-        SUCCEED();
-    }
 
-    TEST_F(asVectorTest,isTEST) {
-        SUCCEED();
-    }
 };
+
+TEST_F(asVectorTest, isEmptyInitially) {
+    SUCCEED();
+}
+
+TEST_F(asVectorTest,isTEST) {
+    SUCCEED();
+}
